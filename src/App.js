32a52596a1f3082cffaf7b3c374c21f21dff7ccf
Crinstaniev <57042057+Crinstaniev@users.noreply.github.com>
2021-06-24 17:07:52 +0800

    edirt

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import sciecon from "./sciecon_blue.png";
+import alpha from "./alpha.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={alpha} alt="sciecon" className="Sci-logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello From <code>Johnathan Zhuang</code> & <code>SciEcon</code>!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/crinstaniev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit My GitHub!
         </a>
       </header>
     </div>
